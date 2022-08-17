1f06f61215e3931879b327cdc1029430b2188d42
11notes <github@11notes.ch>
2022-08-17 13:36:53 +0200

    changed api port from 8080 to 6780

--- rootfs/usr/local/bin/healthcheck.sh
+++ rootfs/usr/local/bin/healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-CURL_RESULT=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"command": "status-get", "service": [ "dhcp4" ]}' 127.0.0.1:8080) || exit 1
+CURL_RESULT=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"command": "status-get", "service": [ "dhcp4" ]}' 127.0.0.1:6780) || exit 1
 if echo "${CURL_RESULT}" | grep -q '"status": "ready"'; then
     exit 0;
 else
